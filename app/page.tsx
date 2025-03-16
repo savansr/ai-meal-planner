47c472a515dfc659aeac3bba3b9f8a988c6ef801
savansr <Savansr26@gmail.com>
2025-03-16 15:19:47 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,5 @@
 // app/page.tsx (HomePage)
 "use client";
-
-import { useState } from "react";
-import { SignIn, SignUp } from "@clerk/nextjs";
 import Link from "next/link";
 
 export default function HomePage() {
